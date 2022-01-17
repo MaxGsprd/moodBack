8dc457994b8ff2703a0be217231690b4567d9495
MaxGsprd <max.gaspard@outlook.fr>
2022-01-17 10:34:56 +0100

    put back EstablishmentRepository

---
+++ src/main/java/com/mood/mood/repository/EstablishmentRepository.java
@@ -0,0 +1,20 @@
+package com.mood.mood.Repository;
+
+import com.mood.mood.model.Establishment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EstablishmentRepository extends JpaRepository<Establishment, Integer> {
+
+    Establishment findById(int id);
+    List<Establishment> findByNameLikeIgnoreCase(String name); //like + ignoreCase query allows finding name like arg with case insensitivity
+    List<Establishment> findByCategoryId(int categoryId);
+
+//    List<Establishment> findByLocalisationLike(String localisation);// a transformer en localisation
+//    Establishment deleteById();
+//    List<Establishment> findByStatus(Boolean status);
+
+}
