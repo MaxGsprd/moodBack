4fa45df49e8965bf130bc4c47b020ce2e3d5d610
MaxGsprd <max.gaspard@outlook.fr>
2022-01-17 08:27:50 +0100

    add moddelmapper java Bean call

--- src/main/java/com/mood/mood/MoodApplication.java
+++ src/main/java/com/mood/mood/MoodApplication.java
@@ -1,10 +1,17 @@
 package com.mood.mood;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoodApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoodApplication.class, args);
