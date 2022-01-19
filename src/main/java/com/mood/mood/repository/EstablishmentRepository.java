89dd39cca07065026f4b4002f1d90ef6de99df4a
MaxGsprd <max.gaspard@outlook.fr>
2022-01-19 11:22:45 +0100

    add deleteById

--- src/main/java/com/mood/mood/repository/EstablishmentRepository.java
+++ src/main/java/com/mood/mood/repository/EstablishmentRepository.java
@@ -10,11 +10,12 @@ import java.util.List;
 @Repository
 public interface EstablishmentRepository extends JpaRepository<Establishment, Integer> {
     Establishment findById(int id);
+    List<Establishment> findAll();
     List<Establishment> findByNameLikeIgnoreCase(String name); //like + ignoreCase query allows finding name like arg with case insensitivity
     List<Establishment> findByCategoryId(int categoryId);
     Establishment findByNameContaining(String name);
+    void deleteById();
+
     Establishment findByLocalisation(String localisation); // a transformer en localisation
-    List<Establishment> findByCategory(Category category);
-//    Establishment deleteById();
 //    List<Establishment> findByStatus(Boolean status);
 }
