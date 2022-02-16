63d997d222cd1e61af3fe26fbf20865de2155434
Max <max.gaspard@outlook.fr>
2022-02-16 11:06:55 +0100

    add comment service interface

--- src/main/java/com/mood/mood/service/ICommentService.java
+++ src/main/java/com/mood/mood/service/ICommentService.java
@@ -1,10 +1,25 @@
 package com.mood.mood.service;
 
+import com.mood.mood.dto.in.CommentForm;
+import com.mood.mood.dto.in.EstablishmentForm;
 import com.mood.mood.dto.out.CommentDetails;
+import com.mood.mood.dto.out.EstablishmentDetails;
+import com.mood.mood.model.Comment;
 import com.mood.mood.model.Establishment;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface ICommentService {
-    List<CommentDetails> getAllCommentsByEstablishment(Establishment establishment) throws Exception;
+    List<CommentDetails> getAllComments() throws Exception;
+    List<CommentDetails> getAllCommentsByEstablishmentId(int establishmentId) throws Exception;
+    List<CommentDetails> getAllCommentsByStatus(Boolean status) throws Exception;
+    List<CommentDetails> getAllCommentsByUserId(int userId) throws Exception;
+    List<CommentDetails> getAllCommentsByGroupType(int groupType) throws Exception;
+//    List<CommentDetails> getAllCommentsByCreatedDate(LocalDateTime createdDate) throws Exception;
+    Comment createComment(@ModelAttribute CommentForm commentForm, int establishment_id, int user_id) throws Exception;
+    void deleteCommentById(int id) throws Exception;
+    CommentDetails updateComment(final int id, CommentForm commentForm) throws Exception;
 }
