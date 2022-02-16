65bb0fc94b2da8b62311eb92070dce304a02e446
Max <max.gaspard@outlook.fr>
2022-02-16 11:09:44 +0100

    modify comment repository

--- src/main/java/com/mood/mood/repository/EstablishmentRepository.java
+++ src/main/java/com/mood/mood/repository/EstablishmentRepository.java
@@ -1,6 +1,5 @@
 package com.mood.mood.repository;
 
-import com.mood.mood.model.Category;
 import com.mood.mood.model.Establishment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,8 +13,8 @@ public interface EstablishmentRepository extends JpaRepository<Establishment, In
     List<Establishment> findByNameLikeIgnoreCase(String name); //like + ignoreCase query allows finding name like arg with case insensitivity
     List<Establishment> findByCategoryId(int categoryId);
     Establishment findByNameContaining(String name);
-    void deleteById();
-
+    void deleteById(int id);
+    List<Establishment> findByStatus(Boolean status);
     Establishment findByLocalisation(String localisation); // a transformer en localisation
-//    List<Establishment> findByStatus(Boolean status);
+
 }
